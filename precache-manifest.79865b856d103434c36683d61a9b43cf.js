6c0dfdf71028a7faf45186f373462dfb15385d3c
Guri Sandhu <guri.sandhu@npadfhsc.com.au>
2019-07-09 22:20:55 +1000

    Updates

---
+++ precache-manifest.79865b856d103434c36683d61a9b43cf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb6eb42a61ce53dc485a9ce53213a2f8",
+    "url": "/space-mission/static/media/space-photo.eb6eb42a.jpeg"
+  },
+  {
+    "revision": "a3c5832620ee23d135eb48a78a70f1b7",
+    "url": "/space-mission/static/media/mission-icon.a3c58326.png"
+  },
+  {
+    "revision": "21dd881b2b653d18808928cfc28419c1",
+    "url": "/space-mission/static/media/mission-icon-2.21dd881b.png"
+  },
+  {
+    "revision": "33cdddf7b9e8f7aa99235b2bf6c90dfe",
+    "url": "/space-mission/static/media/down-chevron.33cdddf7.svg"
+  },
+  {
+    "revision": "15debd804a99ba57277c54d2786d21c2",
+    "url": "/space-mission/static/media/down-chevron.15debd80.svg"
+  },
+  {
+    "revision": "80cc18ff714a91c58b36",
+    "url": "/space-mission/static/js/runtime~main.80cc18ff.js"
+  },
+  {
+    "revision": "e4d9b3d7ed1ea872a890",
+    "url": "/space-mission/static/js/main.e4d9b3d7.chunk.js"
+  },
+  {
+    "revision": "8aa9a39b140bb9e0aa88",
+    "url": "/space-mission/static/js/1.8aa9a39b.chunk.js"
+  },
+  {
+    "revision": "e4d9b3d7ed1ea872a890",
+    "url": "/space-mission/static/css/main.8ac6e8b3.chunk.css"
+  },
+  {
+    "revision": "6190bb17524117f440ef5f3d4faba61f",
+    "url": "/space-mission/index.html"
+  }
+];
