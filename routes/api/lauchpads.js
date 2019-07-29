8e9486611b202c5be6294408bb7fb567dde9c36c
Guri Sandhu <contactguri@gmail.com>
2019-07-29 12:04:44 +1000

    remove faviocon

--- routes/api/lauchpads.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const data = require('../../resources/launchpads.json');
-
-router.get('/', (req, res) => {
-	res.json(data);
-});
--- routes/api/launches.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const data = require('../../resources/launches.json');
-
-router.get('/', (req, res) => {
-	res.json(data);
-});
